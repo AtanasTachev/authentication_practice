f2e3a6b8b43a81bd54c7590aa79b40f01db21a77
Atanas Tachev <tachev_a#yahoo.com>
2021-10-09 01:32:56 +0300

    get server running

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const env = process.env.NODE_ENV || 'development';
 const app = express();
 const config = require('./config/config')[env];
 const routes = require('./routes');
@@ -10,7 +11,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv/config');
 
-mongoose.connect(process.env.DB.CONNECTION, () => {
+mongoose.connect(process.env.DB_CONNECTION, () => {
     console.log('Connected to DB ...');
 });
 const db = mongoose.connection;
@@ -18,4 +19,4 @@ db.on('errpr', console.error.bind(console, 'connection error'));
 
 app.use(routes);
 
-app.listen(config,port, () => console.log(`Server running on port ${config.port}`));
+app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
